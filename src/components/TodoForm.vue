28490486c38d1f37861ebc30e91262027a6e566a
GustavoTambos1 <60891441+GustavoTambos1@users.noreply.github.com>
2020-07-06 13:28:58 -0300

    Add files via upload

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="Form">
+  <input id="texto" type="text" placeholder="Nova Tarefa?" v-model="novaTarefa">
+  <button id="botao" @click="adicionarTarefa">OK</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TodoForm",
+    data: function(){
+        return{
+            novaTarefa: ''
+        }
+    }, methods:{
+        adicionarTarefa: function(){
+            this.$emit('addTarefa',{novaTarefa: this.novaTarefa})
+        }
+    }
+}
+</script>
+
+<style>
+    #Form{
+        background-color: grey;
+        width: 600px;
+        height: 200px;
+        margin: 80px 0px 0px 35%;
+        border: 1px black solid;
+        border-radius: 20px;
+    }
+
+    #texto{
+        margin: 80px 0 0 27%;
+        height: 20px;
+        width: 200px;
+        border: black 1px solid;
+    }
+
+    #botao{
+       width: 50px;
+       height: 20px;
+       color: green;
+       outline: none;
+       cursor: pointer;
+       margin: 0px 5px;
+    }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="todoList">
+      <div class="tarefasCaixas" v-for="(todoList, index) in tarefas" :key="index">
+        {{todoList.descricao}}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"TodoList",
+    props:['tarefas']
+}
+</script>
+
+<style>
+    #todoList{
+        background-color: grey;
+        width: 600px;
+        height: 200px;
+        margin: 10px 0px 0px 35%;
+        display: flex;
+        flex-wrap: wrap;
+        overflow: auto;
+        border: black 1px solid;
+        border-radius: 20px;
+    }
+
+    .tarefasCaixas{
+        width: (tarefas);
+        background-color: green;
+        height: 50px;
+        border: 2px solid black;
+        margin: 5px 20px 0 10px;
+        text-align: center;
+        line-height: 50px;
+        cursor: pointer;
+        color: white; 
+        overflow: hidden;
+    }
+</style>
